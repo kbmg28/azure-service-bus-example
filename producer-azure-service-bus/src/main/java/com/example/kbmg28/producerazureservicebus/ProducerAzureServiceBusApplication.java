0266ccc4ae3a57dee1b8ec3533bec7997fc2b05b
Kevin Gomes <kgomes.mazzatech@ext.valid.com>
2022-11-17 13:51:32 -0400

    [ProducerAzureServiceBus]: init

---
+++ producer-azure-service-bus/src/main/java/com/example/kbmg28/producerazureservicebus/ProducerAzureServiceBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.kbmg28.producerazureservicebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerAzureServiceBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerAzureServiceBusApplication.class, args);
+	}
+
+}
---
+++ producer-azure-service-bus/src/test/java/com/example/kbmg28/producerazureservicebus/ProducerAzureServiceBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kbmg28.producerazureservicebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerAzureServiceBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
